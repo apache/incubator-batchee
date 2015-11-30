8750f4bee017f60045bf0e51b2c5c9c1705852a0
Romain Manni-Bucau <rmannibu@gmail.com>
2015-11-30 12:03:19 +0100

    adding CliConfiguration for batchee cli, simpler customization of the cli

--- tools/cli/src/main/java/org/apache/batchee/cli/BatchEECLI.java
+++ tools/cli/src/main/java/org/apache/batchee/cli/BatchEECLI.java
@@ -20,6 +20,7 @@ import io.airlift.airline.Cli;
 import io.airlift.airline.Help;
 import io.airlift.airline.ParseException;
 import org.apache.batchee.cli.command.Abandon;
+import org.apache.batchee.cli.command.CliConfiguration;
 import org.apache.batchee.cli.command.Executions;
 import org.apache.batchee.cli.command.Exit;
 import org.apache.batchee.cli.command.Instances;
@@ -29,56 +30,91 @@ import org.apache.batchee.cli.command.Running;
 import org.apache.batchee.cli.command.Start;
 import org.apache.batchee.cli.command.Status;
 import org.apache.batchee.cli.command.Stop;
+import org.apache.batchee.cli.command.UserCommand;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.ServiceLoader;
 
 import static java.lang.ClassLoader.getSystemClassLoader;
 
 public class BatchEECLI {
     public static void main(final String[] args) {
-        final Cli.CliBuilder<Runnable> builder = Cli.<Runnable>builder("batchee")
-                .withDescription("BatchEE CLI")
-                .withDefaultCommand(Help.class)
-                .withCommands(Help.class,
-                        Names.class,
-                        Start.class, Restart.class,
-                        Status.class, Running.class,
-                        Stop.class, Abandon.class,
-                        Instances.class, Executions.class);
-
-        // user extension point
-        try { // read manually cause we dont want to instantiate them there, so no ServiceLoader
-            final ClassLoader tccl = Thread.currentThread().getContextClassLoader();
-            final ClassLoader loader = tccl != null ? tccl : getSystemClassLoader();
-            final Enumeration<URL> uc = loader.getResources("META-INF/services/org.apache.batchee.cli.command.UserCommand");
-            while (uc.hasMoreElements()) {
-                final URL url = uc.nextElement();
-                BufferedReader r = null;
-                try {
-                    r = new BufferedReader(new InputStreamReader(url.openStream()));
-                    String line;
-                    while ((line = r.readLine()) != null) {
-                        if (line.startsWith("#") || line.trim().isEmpty()) {
-                            continue;
-                        }
-                        builder.withCommand(Class.class.cast(loader.loadClass(line.trim())));
-                    }
-                } catch (final IOException ioe) {
-                    throw new IllegalStateException(ioe);
-                } catch (final ClassNotFoundException cnfe) {
-                    throw new IllegalArgumentException(cnfe);
-                } finally {
-                    if (r != null) {
-                        r.close();
-                    }
-                }
+        final Iterator<CliConfiguration> configuration = ServiceLoader.load(CliConfiguration.class).iterator();
+        final CliConfiguration cliConfiguration = configuration.hasNext() ? configuration.next() : new CliConfiguration() {
+            @Override
+            public String name() {
+                return "batchee";
             }
-        } catch (final IOException e) {
-            throw new IllegalStateException(e);
+
+            @Override
+            public String description() {
+                return "BatchEE CLI";
+            }
+
+            @Override
+            public boolean addDefaultCommands() {
+                return true;
+            }
+
+            @Override
+            public Iterator<Class<? extends UserCommand>> userCommands() {
+                final Collection<Class<? extends UserCommand>> classes = new ArrayList<Class<? extends UserCommand>>();
+                try { // read manually cause we dont want to instantiate them there, so no ServiceLoader
+                    final ClassLoader tccl = Thread.currentThread().getContextClassLoader();
+                    final ClassLoader loader = tccl != null ? tccl : getSystemClassLoader();
+                    final Enumeration<URL> uc = loader.getResources("META-INF/services/org.apache.batchee.cli.command.UserCommand");
+                    while (uc.hasMoreElements()) {
+                        final URL url = uc.nextElement();
+                        BufferedReader r = null;
+                        try {
+                            r = new BufferedReader(new InputStreamReader(url.openStream()));
+                            String line;
+                            while ((line = r.readLine()) != null) {
+                                if (line.startsWith("#") || line.trim().isEmpty()) {
+                                    continue;
+                                }
+                                classes.add(Class.class.cast(loader.loadClass(line.trim())));
+                            }
+                        } catch (final IOException ioe) {
+                            throw new IllegalStateException(ioe);
+                        } catch (final ClassNotFoundException cnfe) {
+                            throw new IllegalArgumentException(cnfe);
+                        } finally {
+                            if (r != null) {
+                                r.close();
+                            }
+                        }
+                    }
+                } catch (final IOException e) {
+                    throw new IllegalStateException(e);
+                }
+                return classes.iterator();
+            }
+        };
+
+        final Cli.CliBuilder<Runnable> builder = Cli.<Runnable>builder(cliConfiguration.name())
+                .withDescription(cliConfiguration.description())
+                .withDefaultCommand(Help.class);
+        if (cliConfiguration.addDefaultCommands()) {
+            builder.withCommands(Help.class,
+                Names.class,
+                Start.class, Restart.class,
+                Status.class, Running.class,
+                Stop.class, Abandon.class,
+                Instances.class, Executions.class);
+        }
+        final Iterator<Class<? extends UserCommand>> userCommands = cliConfiguration.userCommands();
+        if (userCommands != null) {
+            while (userCommands.hasNext()) {
+                builder.withCommand(userCommands.next());
+            }
         }
 
         final Cli<Runnable> parser = builder.build();
