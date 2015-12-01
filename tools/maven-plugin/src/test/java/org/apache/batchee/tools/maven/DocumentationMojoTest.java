e87abcf68da008a061df26d9a0601cb0a71d1421
Romain Manni-Bucau <rmannibu@gmail.com>
2015-12-01 17:10:23 +0100

    adding documentation mojo

---
+++ tools/maven-plugin/src/test/java/org/apache/batchee/tools/maven/DocumentationMojoTest.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.batchee.tools.maven;
+
+import org.apache.batchee.doc.api.Configuration;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.IOUtil;
+import org.junit.Test;
+
+import javax.batch.api.BatchProperty;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class DocumentationMojoTest {
+    @Test
+    public void document() throws MojoFailureException, MojoExecutionException, IOException {
+        final File out = new File("target/DocumentationMojoTest/output.adoc");
+        new DocumentationMojo() {{
+            classes = new File("target/test-classes/org/apache/batchee/tools/maven/");
+            output = out;
+        }}.execute();
+        final FileInputStream fis = new FileInputStream(out);
+        assertEquals("= myComponent\n" +
+            "\n" +
+            "|===\n" +
+            "|Name|Description\n" +
+            "|configByDefault|this is an important config\n" +
+            "|expl|this one is less important\n" +
+            "|===\n" +
+            "\n" +
+            "= org.apache.batchee.tools.maven.batchlet.SimpleBatchlet\n" +
+            "\n" +
+            "|===\n" +
+            "|Name|Description\n" +
+            "|fail|-\n" +
+            "|sleep|-\n" +
+            "|===\n" +
+            "\n", IOUtil.toString(fis));
+        fis.close();
+    }
+
+    @Named
+    public static class MyComponent {
+        @Inject
+        @BatchProperty
+        @Configuration("this is an important config")
+        private String configByDefault;
+
+        @Inject
+        @BatchProperty(name = "expl")
+        @Configuration("this one is less important")
+        private String explicit;
+
+        private String ignored1;
+
+        @BatchProperty
+        private String ignored;
+    }
+}
