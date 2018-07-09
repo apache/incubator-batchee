30aa2a4d7c86a151abd7ddcdeaff7ad1a2be0994
Mark Struberg <struberg@apache.org>
2018-07-09 16:19:07 +0200

    BATCHEE-131 keep track of running threads
    And shut them down if the container stops

--- jbatch/src/main/java/org/apache/batchee/container/services/executor/AbstractThreadPoolService.java
+++ jbatch/src/main/java/org/apache/batchee/container/services/executor/AbstractThreadPoolService.java
@@ -17,15 +17,29 @@
 package org.apache.batchee.container.services.executor;
 
 import org.apache.batchee.container.exception.BatchContainerServiceException;
+import org.apache.batchee.container.util.BatchWorkUnit;
 import org.apache.batchee.spi.BatchThreadPoolService;
 
+import javax.batch.operations.JobOperator;
+import javax.batch.runtime.BatchRuntime;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.apache.batchee.container.util.ClassLoaderAwareHandler.runnableLoaderAware;
 
 public abstract class AbstractThreadPoolService implements BatchThreadPoolService {
+    private final static Logger LOGGER = Logger.getLogger(AbstractThreadPoolService.class.getName());
+
     protected ExecutorService executorService;
+
+    volatile boolean shutdown = false;
+
+    private Set<BatchWorkUnit> runningBatchWorkUnits = Collections.synchronizedSet(new HashSet<BatchWorkUnit>());
 
     protected abstract ExecutorService newExecutorService(Properties batchConfig);
 
@@ -36,17 +50,58 @@ public abstract class AbstractThreadPoolService implements BatchThreadPoolServic
 
     @Override
     public void shutdown() throws BatchContainerServiceException {
+        this.shutdown = true;
+        if (!runningBatchWorkUnits.isEmpty()) {
+            JobOperator jobOperator = BatchRuntime.getJobOperator();
+            for (BatchWorkUnit batchWorkUnit : runningBatchWorkUnits) {
+                try {
+                    long executionId = batchWorkUnit.getJobExecutionImpl().getExecutionId();
+                    if (executionId >= 0) {
+                        jobOperator.stop(executionId);
+                    }
+                } catch (Exception e) {
+                    LOGGER.log(Level.SEVERE, "Failure while shutting down execution", e);
+                }
+            }
+        }
+
         executorService.shutdownNow();
         executorService = null;
     }
 
     @Override
     public void executeTask(final Runnable work, final Object config) {
-        executorService.execute(runnableLoaderAware(work));
+        if (shutdown) {
+            throw new IllegalStateException("Refuse to start Batch Task due to shutdown being in progress!");
+        }
+        executorService.execute(runnableLoaderAware(new ActiveWorkTracker(work)));
     }
 
     @Override
     public String toString() {
         return getClass().getName();
     }
+
+    class ActiveWorkTracker implements Runnable {
+        private final Runnable work;
+
+        ActiveWorkTracker(Runnable work) {
+            this.work = work;
+        }
+
+        @Override
+        public void run() {
+            try {
+                if (work instanceof BatchWorkUnit) {
+                    runningBatchWorkUnits.add((BatchWorkUnit) work);
+                }
+                work.run();
+            } finally {
+                if (work instanceof BatchWorkUnit) {
+                    runningBatchWorkUnits.remove(work);
+                }
+            }
+        }
+    }
+
 }
